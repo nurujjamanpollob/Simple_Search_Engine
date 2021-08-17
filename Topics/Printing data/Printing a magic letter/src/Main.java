c5108a121f115921702bfcaf3034b72d5f5adfa4
Nurujjaman Pollob <nurujjamanpollob@gmail.com>
2021-08-17 18:52:32 +0600

    initial commit - will add more lesons soon

---
+++ Topics/Printing data/Printing a magic letter/src/Main.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) {
+
+        String spaceStr = "      **";
+        String starSpac = "**    **";
+        // put your code here
+        System.out.println("********");
+        System.out.println(spaceStr);
+        System.out.println(spaceStr);
+        System.out.println(spaceStr);
+        System.out.println(starSpac);
+        System.out.println(starSpac);
+        System.out.println(" *******");
+
+    }
+}
